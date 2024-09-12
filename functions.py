57b8186f65254e43da50067fd6e1dfbcda108242
Guilherme Rolim <guilherme.rolim.20221@poli.ufrj.br>
2024-09-12 11:49:46 -0300

    Adição do Dijkstra

--- functions.py
+++ functions.py
@@ -97,6 +97,52 @@ def visualizar_grafo(path):
     plt.title("Graph from File with Edge Weights")
     plt.show()
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5):
+    """
+    If the graph is a tree, this will return the positions to plot this in a
+    hierarchical layout.
+
+    G: the graph (must be a tree)
+    root: the root node of the current branch
+    width: horizontal space allocated for this branch
+    vert_gap: gap between levels of the hierarchy
+    vert_loc: vertical location of root
+    xcenter: horizontal location of root
+    """
+
+    pos = {root: (xcenter, vert_loc)}
+    neighbors = list(G.neighbors(root))
+    if len(neighbors) != 0:
+        dx = width / len(neighbors) 
+        nextx = xcenter - width / 2 - dx / 2
+        for neighbor in neighbors:
+            nextx += dx
+            pos.update(hierarchy_pos(G, root=neighbor, width=dx, vert_gap=vert_gap, vert_loc=vert_loc-vert_gap, xcenter=nextx))
+    return pos
+
+def visualizar_arvore_de_pais(matriz_adj, pais):
+    G = nx.DiGraph()  # Use directed graph to preserve parent-child hierarchy
+
+    # Create edges for each child-parent relationship
+    for filho in pais.keys():
+        G.add_edge(pais[filho], filho, weight=matriz_adj[letter_to_int(pais[filho])-1][letter_to_int(filho)-1])
+
+    # Find the root (the node with no parent)
+    root = [node for node in G.nodes if node not in pais.keys()][0]
+
+    # Get the hierarchical position layout
+    pos = hierarchy_pos(G, root)
+
+    # Draw the graph with the specified layout
+    plt.figure(figsize=(10, 8))  # Adjust the figure size if needed
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', font_weight='bold', node_size=700, edge_color='gray')
+
+    plt.title("Hierarchical Parent-Child Tree")
+    plt.show()
+
 # Retorna os vizinhos do node na matriz adjacencia. Começa em 0
 def obter_vizinhos(node, matriz_adj):
     nodes = []
@@ -106,3 +152,16 @@ def obter_vizinhos(node, matriz_adj):
             nodes.append(i)
     return nodes
 
+def obter_indice_menor_distancia(abertos, distancia):
+    min_dist_idx = 0
+    for i in range(len(abertos)):
+        if distancia[abertos[i]] < distancia[abertos[min_dist_idx]]:
+            min_dist_idx = i
+    return min_dist_idx
+
+# Retorna se um dado vertice é conexo ao grafo
+def conexo_ao_grafo(node, matriz_adj):
+    for i in range(len(matriz_adj[node])):
+        if matriz_adj[node][i] != 0:
+            return True
+    return False
